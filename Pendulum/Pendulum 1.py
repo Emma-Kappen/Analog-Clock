5035a82e6ea192589a1b3f926066e02f8e230a0a
Emma Kappen <95895520+Emma-Kappen@users.noreply.github.com>
2021-12-13 11:55:06 +0530

    Delete Pendulum 1.py

--- Pendulum/Pendulum 1.py
+++
@@ -1,9 +0,0 @@
-# import library
-import pendulum
-dt = pendulum.datetime(2020, 11, 27)
-print(dt)
-
-#local() creates datetime instance with local timezone
-local = pendulum.local(2020, 11,27)
-print(local)
-print(local.timezone.name)
